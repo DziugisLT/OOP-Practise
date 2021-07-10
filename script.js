a2638011a382dfb1f64c162d9eecf2f788864349
Džiugas Balčiūnas <42406609+DziugisLT@users.noreply.github.com>
2021-07-10 22:19:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,57 +1,5 @@
 'use strict';
 
-/* const Person = function (firstName, birthYear) {
-  this.firstName = firstName;
-  this.birthYear = birthYear;
-
-  //  this.calcAge = function () {
-  //   console.log(2037 - this.birthYear);
-  // }; 
-};
-
-const jonas = new Person('Jonas', 1991);
-console.log(jonas);
-
-const matilda = new Person('Matilda', 2017);
-const jack = new Person('Jack', 1975);
-
-console.log(matilda);
-console.log(jack);
-
-console.log(jonas instanceof Person);
-
-Person.prototype.calcAge = function () {
-  console.log(2037 - this.birthYear);
-};
-
-jonas.calcAge();
-matilda.calcAge();
-jack.calcAge();
-
-Person.prototype.species = 'Homo sapiens';
-console.log(jonas.species, matilda.species);
-
-console.log(jonas.__proto__);
-console.log(jonas.__proto__.__proto__);
-console.log(jonas.__proto__.__proto__.__proto__);
-
-console.dir(Person.prototype.constructor);
-
-const arr = [
-  1, 2, 4, 4, 4, 4, 4, 3, 2, 2, 3, 2, 2, 4, 7, 8, 9, 58, 3, 4, 5, 6, 7, 8,
-];
-
-console.log(arr.__proto__.__proto__);
-
-Array.prototype.unique = function () {
-  return [...new Set(this)];
-};
-
-console.log(arr.unique());
-
-const h1 = document.querySelector('h1');
- */
-
 const Car = function (make, speed) {
   this.make = make;
   this.speed = speed;
